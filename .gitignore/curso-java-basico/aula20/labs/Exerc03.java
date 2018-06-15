495bda247063cbd09643f3269c69d1f0180715df
Daniel Barreto <danielbarreto559@gmail.com>
2018-06-15 20:00:41 -0300

    Update Exerc03.java

--- .gitignore/curso-java-basico/aula20/labs/Exerc03.java
+++ .gitignore/curso-java-basico/aula20/labs/Exerc03.java
@@ -3,38 +3,34 @@ import java.util.Scanner;
 public class Exerc03 {
 
 	public static void main(String[] args) {
-		/*
-		 * Capture do teclado valores para preenchimento de uma matriz M
-          3x3. Após a captura imprima a matriz criada e encontre a
-          quantidade de números pares, a quantidade de números ímpares.
-		 */
-		
-		Scanner scan = new Scanner(System.in);
-		int[][] matrizM = new int[3][3];
-		
-		for(int i = 0 ; i < matrizM.length ; i++) {
-			for(int j = 0 ; j < matrizM[i].length; j++) {
-				System.out.println("Informe o valor da posição " + i);
-				matrizM[i][j] = scan.nextInt();
-				
+private static Scanner scan;
+
+	public static void main(String[] args) {
+		scan = new Scanner(System.in);
+		int[][] matrix = new int[3][3];
+		int impar = 0;
+		int par = 0;
+		for(int i =0; i < matrix.length; i++) {
+			for(int j = 0 ; j < matrix[i].length; j++) {
+				System.out.println("Informe um numero");
+				matrix[i][j] = scan.nextInt();
 			}
 		}
-		int impares = 0;
-		int pares = 0;
-		for(int i =0; i < matrizM.length; i++) {
-			for(int j =0; j < matrizM[i].length; j++) {
-				if(matrizM[i][j] % 2 ==0) {
-					pares++;
+		
+		for(int i =0; i < matrix.length; i++) {
+			for(int j =0 ; j < matrix[i].length; j++) {
+				if(matrix[i][j] %2 == 0) {
+					par++;
 				}else {
-					impares++;
+					impar++;
 				}
+				System.out.print(matrix[i][j] + " | ");
 			}
+			System.out.println("  ");
 		}
-		System.out.println("A quantidade de numeros impares são: " + impares);
-		System.out.println("A quantidade de numeros pares são: " + pares);
-		
-		
+		System.out.println("Quantidade de numeros Impares: " + impar);
+		System.out.println("Quantidade de numero Pares: " + par);
+
 	}
+
 }
-
-
